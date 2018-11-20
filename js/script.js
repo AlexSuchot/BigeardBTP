459ee106623173e027c36b84f169b67d0ed4035f
marceaudavid <marceau.david@ynov.com>
2018-11-20 08:20:18 +0100

    fix js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,20 @@
-var n = 1;
-
 function onMove(n) {
 
     var slides = document.getElementsByClassName("slide");
-    if (n > slides.length) {
-        n = 1;
-    }
-    if (n < 1) {
-        n = slides.length;
-    }
+    slides[n].id = "selected";
 
-    slides[n - 1].id = "selected";
-
-    var prev = slides[n - 1].previousElementSibling;
-    if (typeof (prev) != 'undefined' && prev != null) {
+    var prev = slides[n].previousElementSibling;
+    if (prev != null) {
         prev.id = "prev";
-        if (typeof (prev.previousElementSibling) != 'undefined' && prev.previousElementSibling != null) {
+        if (prev.previousElementSibling != null) {
             prev.previousElementSibling.id = "hide-prev";
         }
     }
 
-    var next = slides[n - 1].nextElementSibling;
-    if (typeof (next) != 'undefined' && next != null) {
+    var next = slides[n].nextElementSibling;
+    if (next != null) {
         next.id = "next";
-        if (typeof (next.nextElementSibling) != 'undefined' && next.nextElementSibling != null) {
+        if (next.nextElementSibling != null) {
             next.nextElementSibling.id = "hide-next";
         }
     }
