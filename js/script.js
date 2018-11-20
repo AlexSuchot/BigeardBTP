0a91a855b553cf71d996e0725e99b0943e587cdb
marceaudavid <marceau.david@ynov.com>
2018-11-20 15:58:32 +0100

    regex validate form

--- js/script.js
+++ js/script.js
@@ -19,3 +19,53 @@ function onMove(n) {
         }
     }
 }
+
+function validateForm() {
+    var hasDigit = /\d/g;
+    var hasSpace = /\s/;
+    var hasSpecial = /[-\/\\&,@!:;~"#_`²$£€%µ%¨§*+=?.,<>()|[\]{}]/g;
+    var fn = document.form.firstname;
+    if (hasDigit.test(fn.value)) {
+        alert("Nombre dans prénom");
+        fn.focus();
+        return false;
+    }
+    if (hasSpace.test(fn.value)) {
+        alert("Espace dans prénom");
+        fn.focus();
+        return false;
+    }
+    if (hasSpecial.test(fn.value)) {
+        alert("Caractères spécial dans prénom");
+        fn.focus();
+        return false;
+    }
+    var ln = document.form.lastname.value;
+    if (hasDigit.test(ln.value)) {
+        alert("Nombre dans nom");
+        fn.focus();
+        return false;
+    }
+    if (hasSpace.test(ln.value)) {
+        alert("Espace dans nom");
+        fn.focus();
+        return false;
+    }
+    if (hasSpecial.test(ln.value)) {
+        alert("Caractères spécial dans prénom");
+        fn.focus();
+        return false;
+    }
+    var em = document.form.email.value;
+    if (hasDigit.test(em.value)) {
+        alert("Nombre dans email");
+        fn.focus();
+        return false;
+    }
+    if (hasSpace.test(em.value)) {
+        alert("Espace dans email");
+        fn.focus();
+        return false;
+    }
+    return true;
+}
