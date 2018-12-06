29108c17b5224d33492498a3c2c5d0bfd320e694
AlexS <alexandre.suchot@ynov.com>
2018-12-06 15:16:06 +0100

    final form mail

--- mail.php
+++ mail.php
@@ -1,30 +1,38 @@
 <?php
-if(isset($_POST['form'])) {
+//if "email" variable is filled out, send email
+if (isset($_POST['email']))  {
 
 
-    if(!isset($_POST['firstname']) ||
-        !isset($_POST['lastname']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['message'])) {
-        died('Error.');
+    //Email information
+    $admin_email = "bigeard-btp@aventu.re";
+    $email = $_POST['email'];
+    $subject = "Bigeard-BTP formulaire";
+    $comment = $_POST['message'] + " " + $_POST['firstname'] + " " + $_POST['lastname'];
+    $rexSafety = "/[\^<,\"@\/\{\}\(\)\*\$%\?=>:\|;#]+/i";
+
+    if(preg_match($rexSafety,$_POST['firstname'])){
+        echo "Wrong value for the firstname";
+    }
+    else{
+
+    }
+    if(preg_match($rexSafety,$_POST['lastname'])){
+        echo "Wrong value for the lastname";
+    }
+    else{
+
     }
 
-    $to = "alexandre.suchot@gmail.com";
-    $subject = 'test';
-    $message = $_POST['message'];
-    $header = "Content-type: text/html; charset=utf-8 \r\n";
-    $header .= "From: ".$mail." \r\n";
-    $header .= "MIME-Version: 1.0 \r\n";
-    $header .= "Content-Transfer-Encoding: 8bit \r\n";
-    $header .= "Date: ".date("r (T)")." \r\n";
+    //send email
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        mail($admin_email, "$subject", $comment, "From:" . $email);
+        header('Location: http://bigeard-btp.aventu.re/');
 
 
-    mail($to, $subject, $message, $header);
-
-    echo 'email sent.';
-    redirect('index.html');
-
-
-
+        //Email response
+        echo "Thank you for contacting us!";
+    }
+    else {
+        echo "Wrong value for the mail.";
+    }
 }
-?>
